885ae7460fa1a0afd5406b0b3b23623e5227ee7f
CarlottaSeal <carlottak16mars@gmail.com>
2024-10-12 18:24:55 -0500

    nearbyhandler更新

--- Assets/Scripts/Core/Cube/Event/PlayerNearbyEvent.cs
+++ Assets/Scripts/Core/Cube/Event/PlayerNearbyEvent.cs
@@ -5,10 +5,12 @@ namespace Core.Cube.Event
     public class PlayerNearbyEvent
     {
         public GameObject player;
+        public GameObject cube;
 
-        public PlayerNearbyEvent(GameObject player)
+        public PlayerNearbyEvent(GameObject player, GameObject cube)
         {
             this.player = player;
+            this.cube = cube;
         }
     }
 }
--- Assets/Scripts/Core/Cube/Handler/PlayerNearbyHandler.cs
+++ Assets/Scripts/Core/Cube/Handler/PlayerNearbyHandler.cs
@@ -16,6 +16,13 @@ namespace Core.Cube.Handler
         public void OnPlayerNearbyEvent(PlayerNearbyEvent playerNearbyEvent)
         {
             //暂空
+            Vector3 cubePosition = playerNearbyEvent.cube.transform.position;
+            Vector3 playerPosition = playerNearbyEvent.player.transform.position;
+            
+            float distance = Vector3.Distance(cubePosition, playerPosition);
+            
+             // 打印距离（或根据距离执行其他操作
+             Debug.Log("Distance between Cube and Player: " + distance);
         }
 
         ~PlayerNearbyHandler()
