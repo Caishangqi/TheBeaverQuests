66cfe11bfb07323a1d5b2769ca77919a1b7650bd
Shangqi Cai <2470159730@qq.com>
2024-11-18 11:56:09 -0600

    Improve the DynamicSortingOrder.cs that execute script by event

--- Assets/Scripts/Miscellaneous/Visual/DynamicSortingOrder.cs
+++ Assets/Scripts/Miscellaneous/Visual/DynamicSortingOrder.cs
@@ -1,3 +1,5 @@
+using Core.Game;
+using Core.Game.Events;
 using UnityEngine;
 
 namespace Miscellaneous.Visual
@@ -6,13 +8,56 @@ namespace Miscellaneous.Visual
     public class DynamicSortingOrder : MonoBehaviour
     {
         private SpriteRenderer spriteRenderer;
-    
+        private bool needUpdate = false;
+
         void Start()
         {
             spriteRenderer = GetComponent<SpriteRenderer>();
+
+            if (Application.isPlaying)
+            {
+                GameGenericEvent.ColliderDisableEvent += OnColliderDisableEvent;
+                GameGenericEvent.ColliderEnableEvent += OnColliderEnableEvent;
+            }
         }
-        
+
+        private void OnDestroy()
+        {
+            if (Application.isPlaying)
+            {
+                GameGenericEvent.ColliderDisableEvent -= OnColliderDisableEvent;
+                GameGenericEvent.ColliderEnableEvent -= OnColliderEnableEvent;
+            }
+        }
+
+        private void OnColliderEnableEvent(ColliderEnableEvent colliderEnableEvent)
+        {
+            if (colliderEnableEvent.gameObject == gameObject)
+            {
+                needUpdate = false;
+                UpdateSortingOrder();
+            }
+        }
+
+        private void OnColliderDisableEvent(ColliderDisableEvent colliderDisableEvent)
+        {
+            if (colliderDisableEvent.gameObject == gameObject)
+                needUpdate = true;
+        }
+
         void Update()
+        {
+            if (!Application.isPlaying)
+            {
+                UpdateSortingOrder();
+            }
+            else if (needUpdate)
+            {
+                UpdateSortingOrder();
+            }
+        }
+
+        void UpdateSortingOrder()
         {
             spriteRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
         }
